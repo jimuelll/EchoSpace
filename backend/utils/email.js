cb8ebf9f014514f7a40dc56dd1760798137c3018
jimuelll <untalanjimueljean@gmail.com>
2025-11-08 19:50:29 +0800

    email fix logic ver2

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -5,7 +5,7 @@ export const sendVerificationEmail = async (email, code) => {
   try {
     await sgMail.send({
       to: email,
-      from: "noreply@echospace.app", // must be verified or domain-authenticated
+      from: "echospace.authentication@gmail.com", // must be verified or domain-authenticated
       subject: "EchoSpace Verification Code",
       text: `Your verification code is: ${code}`,
     });
