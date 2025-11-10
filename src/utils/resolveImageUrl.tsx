67a3eda8fe4b44f0af0489ad201c0f23d54e9af3
jimuelll <untalanjimueljean@gmail.com>
2025-11-10 23:43:48 +0800

    resolveimageurl changes

--- src/utils/resolveImageUrl.tsx
+++ src/utils/resolveImageUrl.tsx
@@ -2,6 +2,9 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 
 export function resolveImageUrl(url?: string) {
   if (!url) return `${BASE_URL}/default-avatar.svg`;
+  if (url.startsWith("https//")) url = url.replace("https//", "https://");
+  if (url.startsWith("http//")) url = url.replace("http//", "http://");
   if (url.startsWith("http://") || url.startsWith("https://")) return url;
+
   return `${BASE_URL}${url}`;
 }
